003eca880fedae213ccbada8e59e7bc67be4f776
Vinayak Iyer <vinayak.iyer@theflyy.com>
2022-09-19 17:06:11 +0530

    Method description added

--- src/App.js
+++ src/App.js
@@ -34,6 +34,8 @@ function App() {
         ext_user_token: "<user-token-from-partner-api>",
         attachMode: 'popup',
         //attachMode: 'drawer',
+        //attachMode: 'chatbox',
+        //attachMode: 'embed',s
         environment: "STAGING"
     };
     (function () {
@@ -97,8 +99,16 @@ function App() {
             <div className="container top-container">
 
                 <h2>
-                    Initialize the Flyy Web SDK using the consturctor
+                    Flyy Web SDK 
                 </h2>
+
+                <div className={"mt-2"}>
+                <h4>Install the Package using npm</h4>
+                </div>
+                
+                <CodeBlock text={"npm i fly-web-sdk"}/>
+
+                <h4>Initialize the Package using the consturctor</h4>
                 <div className={"code-block-init"}>
                     <CodeBlock
                         text={code}
@@ -107,6 +117,7 @@ function App() {
                         theme={dracula} />
                 </div>
 
+                <h4>Following Methods are availabe for various screens to call.</h4>
                 <div className="app d-flex flex-wrap mb-3">
 
                     <div className={"card m-2"} style={{ width: 20 + 'rem' }}>
@@ -117,6 +128,7 @@ function App() {
                                 text={"flyySDK.clickToOpenSDK(data)"}
                                 language={language}
                                 theme={dracula} />
+                                <p className={"card-text"}>This Method is to Open Offers Screen.</p>
                             <button onClick={() => flyySDK.clickToOpenSDK(data)} className={"form-control btn-primary mt-2  submit-button"} >Offers</button>
                         </div>
                     </div>
@@ -129,6 +141,7 @@ function App() {
                                 text={"flyySDK.openSpinTheWheel()"}
                                 language={language}
                                 theme={dracula} />
+                                <p className={"card-text"}>This Method is to Open the Spin the Wheel Screen. It will only open when there is a live offer is present</p>
                             <button onClick={() => flyySDK.openSpinTheWheel()} className={"form-control btn-primary mt-2 submit-button"} >Spin Wheel</button>
                         </div>
                     </div>
@@ -143,6 +156,7 @@ function App() {
                                 text={"flyySDK.openWalletScreen()"}
                                 language={language}
                                 theme={dracula} />
+                                <p className={"card-text"}>This Method is to Open the Wallet Screen.</p>
                             <button onClick={() => flyySDK.openWalletScreen()} className={"form-control btn-primary mt-2 submit-button"} >Open Wallet</button>
                         </div>
                     </div>
@@ -155,6 +169,7 @@ function App() {
                                 text={"flyySDK.openAccountScreen()"}
                                 language={language}
                                 theme={dracula} />
+                                <p className={"card-text"}>This Method is to Open the Account Screen.</p>
                             <button onClick={() => flyySDK.openAccountScreen()} className={"form-control btn-primary mt-2 submit-button"} >Account</button>
                         </div>
                     </div>
@@ -167,6 +182,7 @@ function App() {
                                 text={"flyySDK.openRewardsScreen()"}
                                 language={language}
                                 theme={dracula} />
+                                <p className={"card-text"}>This Method is to Open the User Rewards Screen.</p>
                             <button onClick={() => flyySDK.openRewardsScreen()} className={"form-control btn-primary mt-2 submit-button"} >Open Rewards</button>
                         </div>
                     </div>
@@ -179,6 +195,7 @@ function App() {
                                 text={"flyySDK.openGiftCardsScreen()"}
                                 language={language}
                                 theme={dracula} />
+                                <p className={"card-text"}>This Method is to Open the Gift Cards Redemption Screen.</p>
                             <button onClick={() => flyySDK.openGiftCardsScreen()} className={"form-control btn-primary mt-2 submit-button"} >Gift Cards</button>
                         </div>
                     </div>
@@ -191,6 +208,7 @@ function App() {
                                 text={"flyySDK.openTournamentsScreen()"}
                                 language={language}
                                 theme={dracula} />
+                                <p className={"card-text"}>This Method is to Open the Games and Tournaments Screen.</p>
                             <button onClick={() => flyySDK.openTournamentsScreen()} className={"form-control btn-primary mt-2 submit-button"} >Tournaments</button>
                         </div>
                     </div>
@@ -203,6 +221,7 @@ function App() {
                                 text={"flyySDK.openReferralscreen()"}
                                 language={language}
                                 theme={dracula} />
+                                <p className={"card-text"}>This Method is to Open the Referrals Screen, where the user's unique Referral code is shown.</p>
                             <button onClick={() => flyySDK.openReferralscreen()} className={"form-control btn-primary mt-2 submit-button"} >Referrals</button>
                         </div>
                     </div>
@@ -215,55 +234,59 @@ function App() {
                                 text={"flyySDK.openQuizZoneScreen()"}
                                 language={language}
                                 theme={dracula} />
+                                <p className={"card-text"}>This Method is to Open the Quiz Listing Screen</p>
                             <button onClick={() => flyySDK.openQuizZoneScreen()} className={"form-control btn-primary mt-2 submit-button"} >Quiz</button>
                         </div>
                     </div>
 
                     <div className={"card m-2"} style={{ width: 20 + 'rem' }}>
-                        <h5 className={"card-header"}>Offers Method</h5>
+                        <h5 className={"card-header"}>Transactions Method</h5>
                         <div className={"card-body"}>
                             <h6 className={"card-subtitle mb-2 text-muted"}>To Open Transactions Screen</h6>
                             <CodeBlock
                                 text={"flyySDK.openTransactionsScreen('Curency type as Param')"}
                                 language={language}
                                 theme={dracula} />
-                            <p className={"card-text"}>flyySDK.openTransactionsScreen('Curency type as Param')</p>
+                            <p className={"card-text"}>Params Accepted - 'String' : Name of the currency to which the transaction needs to be shown</p>
                             <button onClick={() => flyySDK.openTransactionsScreen('Cash')} className={"form-control btn-primary mt-2 submit-button"} >Transation</button>
                         </div>
                     </div>
 
                     <div className={"card m-2"} style={{ width: 20 + 'rem' }}>
-                        <h5 className={"card-header"}>Offers Method</h5>
+                        <h5 className={"card-header"}>Transactions Method</h5>
                         <div className={"card-body"}>
                             <h6 className={"card-subtitle mb-2 text-muted"}>To Open Transactions Screen</h6>
                             <CodeBlock
                                 text={"flyySDK.openTransactionsScreen('Curency type as Param')"}
                                 language={language}
                                 theme={dracula} />
+                                <p className={"card-text"}>Params Accepted - 'String' : Name of the currency to which the transaction needs to be shown</p>
                             <button onClick={() => flyySDK.openTransactionsScreen('Coins')} className={"form-control btn-primary mt-2 submit-button"} >Points History</button>
                         </div>
                     </div>
 
                     <div className={"card m-2"} style={{ width: 20 + 'rem' }}>
-                        <h5 className={"card-header"}>Offers Method</h5>
+                        <h5 className={"card-header"}>Invite and Earn Method</h5>
                         <div className={"card-body"}>
                             <h6 className={"card-subtitle mb-2 text-muted"}>To Open Invite and Earn Screen</h6>
                             <CodeBlock
                                 text={"flyySDK.openInviteAndEarnOffer()"}
                                 language={language}
                                 theme={dracula} />
+                                <p className={"card-text"}>It will only work when there is any live offer available</p>
                             <button onClick={() => flyySDK.openInviteAndEarnOffer()} className={"form-control btn-primary mt-2 submit-button"} >Invite And Earn</button>
                         </div>
                     </div>
 
                     <div className={"card m-2"} style={{ width: 20 + 'rem' }}>
-                        <h5 className={"card-header"}>Offers Method</h5>
+                        <h5 className={"card-header"}>CheckIn Method</h5>
                         <div className={"card-body"}>
                             <h6 className={"card-subtitle mb-2 text-muted"}>To Open CheckIn Screen</h6>
                             <CodeBlock
                                 text={"flyySDK.openDailyCheckInOffer()"}
                                 language={language}
                                 theme={dracula} />
+                                <p className={"card-text"}>It will only work when there is any live offer available</p>
                             <button onClick={() => flyySDK.openDailyCheckInOffer()} className={"form-control btn-primary mt-2 submit-button"} >Daily CheckIn</button>
                         </div>
                     </div>
